1c3f467368f577b2d45d5b96f5e8f8dca6ddadae
sokinpui <sosokinpui@gmail.com>
2025-11-05 10:08:47 +0800

    add zsh script

--- compose.zsh
+++ compose.zsh
@@ -1,12 +1,18 @@
-#!/usr/bin/zsh
+#!/usr/bin/env zsh
 
-./reset_network.sh
+sudo ip link set radarConnection down
+sudo ip link delete radarConnection
 
 echo "Write your parent interface name"
 read parent
 
-./setup_network.sh $parent
-./build.sh
+#sudo ip link add link enx6c1ff708fccc name radarConnection type vlan id 19
+sudo ip link add link $parent name radarConnection type vlan id 19
+sudo ip addr add 10.13.1.166/24 dev radarConnection
+sudo ip link set radarConnection up
+sudo ip route replace default via 10.13.1.1 dev radarConnection
+
+colcon build --packages-select ars548_driver ars548_messages
 
 source install/setup.zsh
 ros2 launch ars548_driver ars548_launch.xml
